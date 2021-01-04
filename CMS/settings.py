be2e893a7f84a0206d1003489cdd2ea9b2b580a3
Rahul Kumar Agrawal <68812899+rahulagrawal01@users.noreply.github.com>
2021-01-04 18:35:17 +0530

    Update settings.py

--- CMS/settings.py
+++ CMS/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1^8kbyp(nrch3ny=(!bnx6i#yk!-lrelm$^t08x)*($0#@jy$u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
